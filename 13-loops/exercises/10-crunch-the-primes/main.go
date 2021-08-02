f58e0a9c44256053c1c7de87eddea61f91f889db
wchen928 <lchen.msc2019@ivey.ca>
2021-08-02 11:28:25 -0700

    label exercises

--- 13-loops/exercises/10-crunch-the-primes/main.go
+++ 13-loops/exercises/10-crunch-the-primes/main.go
@@ -8,6 +8,12 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Crunch the primes
 //
@@ -34,4 +40,37 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+main:
+	for _, arg := range os.Args[1:] {
+		n, err := strconv.Atoi(arg)
+		if err != nil {
+			continue
+		}
+
+		switch {
+		// prime
+		case n == 2 || n == 3:
+			fmt.Print(n, " ")
+			continue
+
+		// not a prime
+		case n <= 1 || n%2 == 0 || n%3 == 0:
+			continue
+		}
+
+		for i, w := 5, 2; i*i <= n; {
+			// not a prime
+			if n%i == 0 {
+				continue main
+			}
+
+			i += w
+			w = 6 - w
+		}
+
+		// all checks ok: it's a prime
+		fmt.Print(n, " ")
+	}
+
+	fmt.Println()
 }
